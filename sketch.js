23a04cd8050c47ab26c9a64d7a7dd2ae2dc0fd0a
tequilaroleplay <91856754+tequilaroleplay@users.noreply.github.com>
2021-12-06 20:25:01 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var retFixo, retMovendo;
+function setup() {
+  createCanvas(1200, 800);
+  retFixo = createSprite(600, 400, 50, 80);
+  retFixo.shapeColor = "green";
+  retFixo.debug = true;
+  retMovendo = createSprite(400, 200, 80, 30);
+  retMovendo.shapeColor = "green";
+  retMovendo.debug = true;
+}
+function draw() {
+  background(0, 0, 0);
+  retMovendo.x = World.mouseX;
+  retMovendo.y = World.mouseY;
+  if (retMovendo.x - retFixo.x < retFixo.width / 2 + retMovendo.width / 2 
+    && retFixo.x - retMovendo.x < retFixo.width / 2 + retMovendo.width / 2 
+    && retMovendo.y - retFixo.y < retFixo.width / 2 + retMovendo.width / 2 
+    && retFixo.y - retMovendo.y < retFixo.width / 2 + retMovendo.width / 2) {
+      
+    retMovendo.shapeColor = "red";
+    retFixo.shapeColor = "red";
+  } else {
+    retMovendo.shapeColor = "green";
+    retFixo.shapeColor = "green";
+  }
+  drawSprites();
+}
